5febb69e43c3e156b8e785d07be4984a49db99fb
Karol Kazmierczak <kazmierczakkarol1999@gmail.com>
2020-04-29 17:29:02 +0200

    dodanie predkosciowej metody varleta

--- oscylator/oscylato.c
+++ oscylator/oscylato.c
@@ -55,7 +55,7 @@ void podstawowa_varleta(){
     rPlus[0] = r[0]+v[0]*dt + F[0]/(2*m) * dt *dt;
     rPlus[1] = r[1]+v[1]*dt + F[1]/(2*m) * dt *dt;
 
-    FILE* wp = fopen("euler.txt", "w");
+    FILE* wp = fopen("pod_varlera.txt", "w");
     int i = 0;
     fprintf(wp,"# \t\t\tt \t\t\tx \t\t\ty \t\t\tvx \t\t\tvy\t\tfx\t\tfy \t\t\tec\n");
     while (t0 <= tn)
@@ -75,8 +75,45 @@ void podstawowa_varleta(){
         float v2 = v[0] * v[0] + v[1]*v[1];
         float r2 = r[0] * r[0] + r[1]*r[1];
         ec = (m*v2)/2 + (k*r2)/2;
+       
 
     }
+    fclose(wp);
+}
+
+int metoda_predkosciowa(){
+    float fx = -k*r[0];
+    float fy = -k*r[1];
+    float F[2] = {fx, fy};
+    float FOld[2];
+    float t0 = 0;
+    float ec;
+    const float tn = 5;
+    int i = 0;
+
+    FILE* wp = fopen("pred_varlera.txt", "w");
+    int i = 0;
+    fprintf(wp,"# \t\t\tt \t\t\tx \t\t\ty \t\t\tvx \t\t\tvy\t\tfx\t\tfy \t\t\tec\n");
+   
+    while (t0 < tn)
+    {
+        FOld[0] = F[0];
+        FOld[1] = F[1];
+        F[0] = -k*r[0];
+        F[1] = -k*r[1];
+        t0 += dt;
+        r[0] += v[0] + FOld[0]/(2*m) * powf(dt, 2);
+        r[1] += v[1] + FOld[1]/(2*m) * powf(dt, 2);
+        v[0] += (F[0]+ FOld[0])/(2*m) * dt;
+        v[0] += (F[0]+ FOld[0])/(2*m) * dt;
+        v[1] += (F[1]+ FOld[1])/(2*m) * dt;
+        float v2 = v[0] * v[0] + v[1]*v[1];
+        float r2 = r[0] * r[0] + r[1]*r[1];
+        ec = (m*v2)/2 + (k*r2)/2;
+         fprintf(wp ,"%d  %f  %e  %e  %e  %e  %e %e %e\n", i, t0, r[0], r[1], v[0], v[1], F[0], F[1], ec);
+        i++;
+    }
+    fclose(wp);
     
 }
 
@@ -88,6 +125,8 @@ int main(int argc, char const *argv[])
     scanf("%d", &dec);
     if(dec==1){
         euler();
+    }else if(dec == 2){
+        podstawowa_varleta();
     }
 
     return 0;
